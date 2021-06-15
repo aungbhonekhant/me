70c934187b43c6d03d84c398a22f5aeb2e3b5234
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2021-06-15 22:12:39 +0630

    change home page path name

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             <div className="main-content">
                 <div className="content">
                     <Switch>
-                        <Route path="/" exact>
+                        <Route path="/me" exact>
                             <HomePage />
                         </Route>
                         <Route path="/about">
