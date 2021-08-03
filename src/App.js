0b1665140e4eb0260af5558b81135dc4ed4bfcc7
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2021-08-03 14:51:32 +0630

    rechange to original

--- src/App.js
+++ src/App.js
@@ -41,6 +41,9 @@ function App() {
                             <Route path="/" exact>
                                 <HomePage />
                             </Route>
+                            <Route path="/me" exact>
+                                <Redirect to="/"/>
+                            </Route>
                             <Route path="/about">
                                 <AboutPage />
                             </Route>
