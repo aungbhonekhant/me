b8d4f4f89655a32b27249ca5644500b8df0e66ab
aungbhonekhant <aungbhonekhant.dev@gmail.com>
2021-08-03 12:30:32 +0630

    add apple loco and favacon icon and loder in all page

--- src/App.js
+++ src/App.js
@@ -6,50 +6,64 @@ import AboutPage from './Pages/AboutPage';
 import PortfoliosPage from './Pages/PortfoliosPage';
 // import BlogsPage from './Pages/BlogsPage';
 import ContactPage from './Pages/ContactPage';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Loader from './Components/Loader';
 
 function App() {
     const [navToggle, setNavToggle] = useState(false);
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 2000)
+    }, [])
 
     const navClick = () => {
         setNavToggle(!navToggle);
+        setLoading(true);
+        setTimeout(() => setLoading(false), 2000)
     }
 
     return (
-        <div className="App">
-            <div className={`sidebar ${ navToggle ? 'nav-toggle' : '' }`}>
-                <NavBar navClick={navClick} />
-            </div>
-            <div className="nav-btn" onClick={navClick}>
-                <div className="lines-1"></div>
-                <div className="lines-2"></div>
-                <div className="lines-3"></div>
-            </div>
-            <div className="main-content">
-                <div className="content">
-                    <Switch>
-                        <Route path="/" exact>
-                            <HomePage />
-                        </Route>
-                        <Route path="/me" exact>
-                            <Redirect to="/"/>
-                        </Route>
-                        <Route path="/about">
-                            <AboutPage />
-                        </Route>
-                        <Route path="/portfolios">
-                            <PortfoliosPage />
-                        </Route>
-                        {/* <Route path="/blogs">
-                            <BlogsPage />
-                        </Route> */}
-                        <Route path="/contact">
-                            <ContactPage />
-                        </Route>
-                    </Switch>
+        <>
+            {loading === false ? (
+            <div className="App">
+                <div className={`sidebar ${ navToggle ? 'nav-toggle' : '' }`}>
+                    <NavBar navClick={navClick} />
+                </div>
+                <div className="nav-btn" onClick={navClick}>
+                    <div className="lines-1"></div>
+                    <div className="lines-2"></div>
+                    <div className="lines-3"></div>
+                </div>
+                <div className="main-content">
+                    <div className="content">
+                        <Switch>
+                            <Route path="/" exact>
+                                <HomePage />
+                            </Route>
+                            <Route path="/me" exact>
+                                <Redirect to="/"/>
+                            </Route>
+                            <Route path="/about">
+                                <AboutPage />
+                            </Route>
+                            <Route path="/portfolios">
+                                <PortfoliosPage />
+                            </Route>
+                            {/* <Route path="/blogs">
+                                <BlogsPage />
+                            </Route> */}
+                            <Route path="/contact">
+                                <ContactPage />
+                            </Route>
+                        </Switch>
+                    </div>
                 </div>
             </div>
-        </div>
+            ) : (
+                <Loader />
+            )}
+        </>
     );
 }
 
---
+++ src/Components/Loader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/loader.css';
+
+function Loader() {
+    return (
+        <div className="ring">
+            Loading
+            <span></span>
+        </div>
+    )
+}
+
+export default Loader
